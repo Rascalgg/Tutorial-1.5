93616baa3062400ee361bab05cdca803aac9a9a1
Rascalgg <kellen.cole10@gmail.com>
2022-10-04 20:46:12 -0400

    Animation commit

--- UnityTutorial1.5/Assets/Scripts/AudioScript.cs
+++ UnityTutorial1.5/Assets/Scripts/AudioScript.cs
@@ -10,11 +10,13 @@ public class AudioScript : MonoBehaviour
     public AudioClip musicClipOne;
     
     public AudioClip musicClipTwo;
+
+    Animator anim;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,22 +26,26 @@ public class AudioScript : MonoBehaviour
         {
             musicSource.clip = musicClipOne;
             musicSource.Play();
+            anim.SetInteger("State", 1);
         }
 
         if(Input.GetKeyUp(KeyCode.W))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             musicSource.clip = musicClipTwo;
             musicSource.Play();
+            anim.SetInteger("State", 2);
         }
 
         if(Input.GetKeyUp(KeyCode.R))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
         }
     }
 }
